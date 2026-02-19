189771acff08fa35c1f1635221b97e4fc23a9257
Erza <erzaamerovci@gmail.com>
2026-02-19 22:33:10 +0100

    fixed logo and icon

--- frontend/src/components/TopBar.vue
+++ frontend/src/components/TopBar.vue
@@ -1,10 +1,11 @@
 <template>
   <header class="topbar">
     <div class="topbar-left">
-      <svg class="logo-icon" viewBox="0 0 24 24">
-        <path d="M10 15l5.19-3L10 9v6m11.56-7.83c.13.47.22 1.1.28 1.9.07.8.1 1.49.1 2.09L22 12c0 2.19-.16 3.8-.44 4.83-.25.9-.83 1.48-1.73 1.73-.47.13-1.33.22-2.65.28-1.3.07-2.49.1-3.59.1L12 19c-4.19 0-6.8-.16-7.83-.44-.9-.25-1.48-.83-1.73-1.73-.13-.47-.22-1.1-.28-1.9-.07-.8-.1-1.49-.1-2.09L2 12c0-2.19.16-3.8.44-4.83.25-.9.83-1.48 1.73-1.73.47-.13 1.33-.22 2.65-.28 1.3-.07 2.49-.1 3.59-.1L12 5c4.19 0 6.8.16 7.83.44.9.25 1.48.83 1.73 1.73z" />
-      </svg>
-      <span class="logo-text">TV Schedule</span>
+   <img
+        src="../assets/logo.png"
+        alt="App Logo"
+        class="logo-image"
+      />
     </div>
     <div class="topbar-center">
       <span class="now-label">{{ clockLabel }}</span>
@@ -26,3 +27,11 @@ defineProps({
 
 defineEmits(['openPrefs'])
 </script>
+
+<style scoped>
+.logo-image {
+  height: 200px;       
+  width: auto;
+  object-fit: contain;
+}
+</style>
